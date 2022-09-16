165da7e598f8750ae2070f00520f45302601e50a
Colin Zarzycki <colin@Colins-MacBook-Pro.local>
2022-09-16 14:26:38 -0400

    attempt merge from local machine

--- figs/process.sh
+++ figs/process.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-PYDIR=/Users/cmz5202/Software/ros/ALL_PY/
+PYDIR=/Users/cmz5202/Software/ros/ros/
 
 cp -v ${PYDIR}/output/*_1995_events.pdf .
 cp -v ${PYDIR}/output/*_1995_scatplot.pdf .
 
-cp -v ${PYDIR}/hists/events.pdf .
-cp -v ${PYDIR}/hists/Max_precip.pdf .
-cp -v ${PYDIR}/hists/Max_runoff.pdf .
-cp -v ${PYDIR}/hists/Max_dSWE.pdf .
+cp -v ${PYDIR}/hists/events_95.pdf .
+cp -v ${PYDIR}/hists/Max_precip_95.pdf .
+cp -v ${PYDIR}/hists/Max_runoff_95.pdf .
+cp -v ${PYDIR}/hists/Max_dSWE_95.pdf .
+
+cp -v ${PYDIR}/hists/events_AB.pdf .
+cp -v ${PYDIR}/hists/Max_precip_AB.pdf .
+cp -v ${PYDIR}/hists/Max_runoff_AB.pdf .
+cp -v ${PYDIR}/hists/Max_dSWE_AB.pdf .
 
 cp -v ${PYDIR}/climo/climo_comp_panel_* .
 
